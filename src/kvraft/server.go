6c41554a70e1809744d025c9497c0e500a1bc72a
kirito <3322640054@qq.com>
2026-04-08 23:12:35 +0800

    写到waitExecutor了 明天继续

--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"6.5840/labgob"
 	"6.5840/labrpc"
@@ -30,11 +31,17 @@ type Op struct {
 	Version  int
 }
 
-type Result struct {
-	index int
+type result struct {
 	term  int
-	value int
+	index int
+	value string
 	err   Err
+}
+
+type content struct {
+	version   int
+	lastValue string
+	err       Err
 }
 
 type KVServer struct {
@@ -47,21 +54,88 @@ type KVServer struct {
 	maxRaftState int // snapshot if log grows this big
 
 	// Your definitions here.
-	store     map[string]string   // key - value
-	cache     map[int64]int       // clientId - version
-	waitChans map[int]chan Result // logIndex - op
+	store     map[string]string
+	waitChans map[int64]chan result
+	cache     map[int64]content
 }
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	ch := make(chan result)
+	op := Op{
+		Key:      args.Key,
+		Value:    "",
+		OpStr:    "Get",
+		ClientId: args.ClientId,
+		Version:  args.Version,
+	}
+	go kv.sendRaft(op, ch)
+	// block here
+	res := <-ch
+	close(ch)
+	reply.Value = res.value
+	reply.Err = res.err
 }
 
-func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
+func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	ch := make(chan result)
+	op := Op{
+		Key:      args.Key,
+		Value:    args.Value,
+		OpStr:    args.OpStr,
+		ClientId: args.ClientId,
+		Version:  args.Version,
+	}
+	go kv.sendRaft(op, ch)
+	// block here
+	res := <-ch
+	close(ch)
+	reply.Err = res.err
 }
 
-func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+// attend that map is not thread safe
+func (kv *KVServer) sendRaft(op Op, ch chan result) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	res := result{
+		value: "",
+	}
+	hit, con := kv.isCacheHit(op.ClientId, op.Version)
+	if hit {
+		res.value = con.lastValue
+		res.err = con.err
+		ch <- res
+		return
+	}
+	index, term, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		res.err = ErrWrongLeader
+		ch <- res
+		return
+	}
+	waitChan := kv.makeWaitChan(term, index)
+	go kv.waitExecute(term, index, ch, waitChan)
+}
+
+func (kv *KVServer) waitExecute(term int, index int, ch chan result, waitChan chan result) {
+	stamp := time.Now()
+	select {
+	case res := <- waitChan:
+		ch <- res
+		case
+	}
+}
+
+func (kv *KVServer) makeWaitChan(term int, index int) chan result {
+	waitChan := make(chan result, 1)
+	chanId := getChanId(term, index)
+	kv.waitChans[chanId] = waitChan
+	return waitChan
+}
+
+func (kv *KVServer) deleteWaitChan(term int, index int) {
+
 }
 
 // the tester calls Kill() when a KVServer instance won't
@@ -108,41 +182,78 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxRaftState = maxRaftState
 	// You may need initialization code here.
 	kv.store = make(map[string]string)
-	kv.cache = make(map[int64]int)
-	kv.waitChans = make(map[int]chan Result)
+	kv.waitChans = make(map[int64]chan result)
+	kv.cache = make(map[int64]content)
 	// background running goroutine
 	go kv.executor()
 	return kv
 }
 
-// judge if command should be send to waitChan
-func (kv *KVServer) OpVersionValid(opStr string, clientId int64, version int) bool {
-	if opStr == "Get" {
-		return true
-	}
-	if kv.cache[clientId] >= version {
-		return false
-	}
-	return true
-}
-
-// receive Op from raft, send to correct waitChan
+// receive log from applyCh and execute, send out the result
+// attend that map is not thread safe
 func (kv *KVServer) executor() {
 	for !kv.killed() {
 		msg := <-kv.applyCh
 		kv.mu.Lock()
-		// if msg type is command
 		if msg.CommandValid {
 			op := msg.Command.(Op)
-			if kv.OpVersionValid(op.OpStr, op.ClientId, op.Version) {
-				// TODO update kv.cache
-				kv.cache[op.ClientId] =
+			term := msg.CommandTerm
+			index := msg.CommandIndex
+			res := result{
+				term:  term,
+				index: index,
+				value: "",
+				err:   OK,
 			}
-		}
-		// TODO if msg type is snapshot
-		if msg.SnapshotValid {
-
+			hit, con := kv.isCacheHit(op.ClientId, op.Version)
+			if !hit {
+				switch op.OpStr {
+				case "Get":
+					if v, ok := kv.store[op.Key]; ok {
+						res.value = v
+					} else {
+						res.err = ErrNoKey
+					}
+				case "Put":
+					kv.store[op.Key] = op.Value
+				case "Append":
+					kv.store[op.Key] += op.Value
+				default:
+					panic("unknown op type!")
+				}
+				kv.cache[op.ClientId] = content{
+					version:   op.Version,
+					lastValue: res.value,
+					err:       res.err,
+				}
+			} else {
+				res.value = con.lastValue
+				res.err = con.err
+			}
+			// why not full: each RPC request use the only channel, ensured by term and index
+			// why not closed: before close, goroutine try to get lock
+			if ch, ok := kv.waitChans[getChanId(term, index)]; ok {
+				select {
+				case ch <- res:
+				default:
+					panic("channel is full or closed")
+				}
+			}
 		}
 		kv.mu.Unlock()
 	}
+}
+
+func getChanId(term int, index int) int64 {
+	id := int64(term) << 32
+	id += int64(index)
+	return id
+}
+
+func (kv *KVServer) isCacheHit(clientId int64, version int) (bool, content) {
+	c, ok := kv.cache[clientId]
+	if ok && c.version >= version {
+		return true, c
+	}
+	return false, content{}
 }
